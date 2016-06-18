287ed20d19f8bc239ec654d257d0f37b4322e596
Clay Dowling <clay@lazarusid.com>
2016-06-18 12:50:01 -0400

    Deal with IV and IX

--- src/roman.c
+++ src/roman.c
@@ -1,4 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
 #include "roman.h"
+
+
+/*
+int is_appear_many(unsigned digit)
+{
+	switch(digit) {
+	case 1:
+	case 10:
+	case 100:
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+int is_appear_once(unsigned digit)
+{
+	switch(digit) {
+	case 5:
+	case 50:
+	case 500:
+		return 1;
+	default:
+		return 0;
+	}
+}
+*/
+
+int should_subtract(unsigned left, unsigned right)
+{
+	if (left < right) return 1;
+	return 0;
+}
 
 static unsigned rn_digit(char rn)
 {
@@ -20,13 +55,37 @@ static unsigned rn_digit(char rn)
 	}
 }
 
+static unsigned rn_sumdigits(int *digits)
+{
+	int i;
+	int total = 0;
+
+	for(i=0; digits[i]; ++i) {
+		if (0 == digits[i]) {
+			return INVALID_NUMERAL;
+		}
+		if (should_subtract(digits[i], digits[i+1]))
+			total -= digits[i];
+		else 
+			total += digits[i];
+	}
+
+	if (total < 1) return INVALID_NUMERAL;
+	return (unsigned)total;
+}
+
 unsigned rn_toint(const char* numeral)
 {
-	int total = 0;
-	char *pos = numeral;
+	unsigned pos;
+	int *values;
+	int len;
 
-	for(pos = numeral; *pos; ++pos) {
-		total += rn_digit(*pos);
+	len = strlen(numeral);
+	values = (int*)calloc(len + 1, sizeof(int));
+
+	for(pos = 0; numeral[pos]; ++pos) {
+		values[pos] = rn_digit(numeral[pos]);
 	}
-	return total;
+
+	return rn_sumdigits(values);
 }
--- test/test_complex.c
+++ test/test_complex.c
@@ -12,12 +12,33 @@ START_TEST(rn_toint_givenII_returns2)
 }
 END_TEST
 
+START_TEST(rn_toint_givenVI_returns6)
+{
+	ck_assert_int_eq(rn_toint("VI"), 6);
+}
+END_TEST
+
+START_TEST(rn_toint_givenIV_returns4)
+{
+	ck_assert_int_eq(rn_toint("IV"), 4);
+}
+END_TEST
+
+START_TEST(rn_toint_givenIX_returns9)
+{
+	ck_assert_int_eq(rn_toint("IX"), 9);
+}
+END_TEST
+
 TCase* tcase_complex_numerals(void)
 {
 	TCase *tc;
 
 	tc = tcase_create("Complex Numerals");
 	tcase_add_test(tc, rn_toint_givenII_returns2);
+	tcase_add_test(tc, rn_toint_givenVI_returns6);
+	tcase_add_test(tc, rn_toint_givenIV_returns4);
+	tcase_add_test(tc, rn_toint_givenIX_returns9);
 
 	return tc;
 }
