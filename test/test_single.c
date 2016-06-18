f61fe94e43645d3769a934e1582e5ea795f91212
Clay Dowling <clay@lazarusid.com>
2016-06-18 16:25:13 -0400

    Refactored tests

--- test/test_single.c
+++ test/test_single.c
@@ -5,61 +5,23 @@
 
 #include "../src/roman.h"
 
-START_TEST(rn_toint_givenJ_returnsINVALIDNUMERAL)
-{
-	ck_assert_int_eq(rn_toint("J"), INVALID_NUMERAL);
-}
-END_TEST
+static char *test_value[] = {"J", "I", "V", "X", "L", "C", "D"};
+static int expected_value[] = {INVALID_NUMERAL, 1, 5, 10, 50, 100, 500};
 
-START_TEST(rn_toint_givenI_returns1)
+START_TEST(rn_toint_singledigit_test)
 {
-	ck_assert_int_eq(rn_toint("I"), 1);
-}
-END_TEST
-
-START_TEST(rn_toint_givenV_returns5)
-{
-	ck_assert_int_eq(rn_toint("V"), 5);
-}
-END_TEST
-
-START_TEST(rn_toint_givenX_returns10)
-{
-	ck_assert_int_eq(rn_toint("X"), 10);
-}
-END_TEST
-
-START_TEST(rn_toint_givenL_returns50)
-{
-	ck_assert_int_eq(rn_toint("L"), 50);
-}
-END_TEST
-
-START_TEST(rn_toint_givenC_returns100)
-{
-	ck_assert_int_eq(rn_toint("C"), 100);
-}
-END_TEST
-
-START_TEST(rn_toint_givenD_returns500)
-{
-	ck_assert_int_eq(rn_toint("D"), 500);
+	ck_assert_int_eq(rn_toint(test_value[_i]), expected_value[_i]);
 }
 END_TEST
 
 TCase *tcase_single_digits(void)
 {
-	TCase *tc_convert_single;
+	TCase *tc;
+	int num_cases = sizeof(expected_value) / sizeof(int);
 
-	tc_convert_single = tcase_create("Single Digits");
-	tcase_add_test(tc_convert_single, rn_toint_givenJ_returnsINVALIDNUMERAL);
-	tcase_add_test(tc_convert_single, rn_toint_givenI_returns1);
-	tcase_add_test(tc_convert_single, rn_toint_givenV_returns5);
-	tcase_add_test(tc_convert_single, rn_toint_givenX_returns10);
-	tcase_add_test(tc_convert_single, rn_toint_givenL_returns50);
-	tcase_add_test(tc_convert_single, rn_toint_givenC_returns100);
-	tcase_add_test(tc_convert_single, rn_toint_givenD_returns500);
+	tc= tcase_create("Single Digits");
+	tcase_add_loop_test(tc, rn_toint_singledigit_test, 0, num_cases);
 
-	return tc_convert_single;
+	return tc;
 }
 
